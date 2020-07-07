dda01c7c0d3df323bf1f0c07b35bee28613ebc2f
Jossivon <41332492+Jossivon@users.noreply.github.com>
2020-07-07 16:10:52 -0500

    Add files via upload

---
+++ PalindromoconsumerApplication.java
@@ -0,0 +1,13 @@
+package jibp.palindromoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromoconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromoconsumerApplication.class, args);
+	}
+
+}
